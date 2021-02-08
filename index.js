5fc1b610621e9500d4ebb209bb05660bace4792c
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-08 21:07:44 +0530

    Final

--- index.js
+++ index.js
@@ -16,15 +16,44 @@ const rl = readline.createInterface({
 });
 
 
-// GET: HTTP
+// HTTP requests
 rl.question('What is your name? ', function (name) {
-    rl.question(`What would you liek to do today: (GET/POST/PUT/DELETE)`, function (method) {
+    rl.question(`What would you like to do today: (GET/POST/PUT/DELETE)`, function (method) {
         if (method === "GET") {
             rl.question('What is the request url? ', function (url) {
                 const output = https.get(`${url}`)
                 const writeStream = fs.writeFileSync('output/get_output.txt', JSON.stringify(output));
-                console.log(output);
+            })
+        } else if (method === "POST") {
+            rl.question('What is the request url? ', function (url) {
+                rl.question('Paste the json body here: ', function (data) {
+                    console.log(chalk.green('Request Success!'));
+                    const output = https.post(`${url}`, data)
+                    if (err) {
+                        console.log(chalk.red('Error!!'));
+                    }
+                })
+            })
+        } else if (method === 'PUT') {
+            rl.question('What is the request url? ', function (url) {
+                rl.question('Paste the json body here: ', function (data) {
+                    console.log(chalk.green('Request Success!'));
+                    const output = https.put(`${url}`, data)
+                    if (err) {
+                        console.log(chalk.red('Error!!'));
+                    }
+                })
+            })
+
+        } else if (method === 'DELETE') {
+            rl.question('What is the request url? ', function (url) {
+                const output = https.put(`${url}`)
+                console.log(chalk.delete('Request Success!'));
+                if (err) {
+                    console.log(chalk.red('Error!!'));
+                }
             })
         }
     })
 })
+
